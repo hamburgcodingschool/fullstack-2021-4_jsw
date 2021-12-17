a69a31be47e9ac64e67fc24e041ba13b91e4bbd5
Helder Pereira <phelder@gmail.com>
2021-12-17 19:38:35 +0000

    sortAndFilter

---
+++ code/lesson7/js4-fetchExampleWithSortAndFilter/js/main.js
@@ -0,0 +1,139 @@
+// closure will run as soon as the page loads
+(function () {
+    
+    const listContent = document.querySelector("#listContent");
+    const detailContent = document.querySelector("#detailContent");
+    const selectSort = document.querySelector("#selectSort");
+    const inputFilter = document.querySelector("#inputFilter");
+
+    let result;
+
+    // async closure so we can take advantage of the await syntax
+    (async function() {
+        const url = "https://jsonplaceholder.typicode.com/users";
+        result = await fetchData(url);
+        listContent.innerHTML = createList(result);
+    })();
+
+    listContent.addEventListener("click", function(event) {
+        if (event.target.tagName !== "BUTTON") {
+            return;
+        }
+
+        const index = event.target.dataset.index;
+        detailContent.innerHTML = createDetails(result[index]);
+    });
+
+    selectSort.addEventListener("change", function(event) {
+        sortAndFilter();
+    });
+
+    inputFilter.addEventListener("input", function() {
+        sortAndFilter();
+    });
+
+    function sortAndFilter() {
+        const searchTerm = inputFilter.value;
+        let filteredResult = filterByName(result, searchTerm);
+        
+        if (selectSort.value === "nameAsc") {
+            filteredResult = sortByNameASC(filteredResult);
+
+        } else if (selectSort.value === "nameDesc") {
+            filteredResult = sortByNameDESC(filteredResult);
+
+        } else if (selectSort.value === "userAsc") {
+            filteredResult = sortByUserASC(filteredResult);
+            
+        } else if (selectSort.value === "userDesc") {
+            filteredResult = sortByUserDESC(filteredResult);
+        }  
+
+        listContent.innerHTML = createList(filteredResult);
+    }
+    
+})();
+
+async function fetchData(url) {
+    try {
+        const response = await fetch(url);
+        const result = await response.json();
+
+        return result;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function createList(data) {
+    let list = "<table><tr><th>Name</th><th>Username</th><th></th></tr>";
+    for (let i = 0; i < data.length; i++) {
+        list += "<tr>";
+        list += `<td>${data[i].name}</td>`;
+        list += `<td>${data[i].username}</td>`;
+        list += `<td><button data-index="${i}">details</button></td>`;
+        list += "</tr>";
+    }
+    list += "</table>";
+
+    return list;
+}
+
+function createDetails(data) {
+    let html = `
+    <h3>${data.name}</h3>
+    <ul>
+    <li>${data.address.street}</li>
+    <li>${data.address.suite}</li>
+    <li>${data.address.city}</li>
+    </ul>`;
+
+    return html;
+}
+
+function sortByNameASC(dataArray) {
+    const copyOfDataArray = [...dataArray];
+    copyOfDataArray.sort(function(a, b) {
+        // this could be done better with localeCompare
+        // here to examplify only
+        if (a.name < b.name) {
+            return -1;
+        } else if (a.name > b.name) {
+            return 1;
+        } else {
+            return 0;
+        }
+    });
+    return copyOfDataArray;
+}
+
+function sortByNameDESC(dataArray) {
+    const copyOfDataArray = [...dataArray];
+    copyOfDataArray.sort(function(a, b) {
+        return b.name.localeCompare(a.name);
+    });
+    return copyOfDataArray;
+}
+
+function sortByUserASC(dataArray) {
+    const copyOfDataArray = [...dataArray];
+    copyOfDataArray.sort(function(a, b) {
+        return a.username.localeCompare(b.username);
+    });
+    return copyOfDataArray;
+}
+
+function sortByUserDESC(dataArray) {
+    const copyOfDataArray = [...dataArray];
+    copyOfDataArray.sort(function(a, b) {
+        return b.username.localeCompare(a.username);
+    });
+    return copyOfDataArray;
+}
+
+function filterByName(dataArray, searchTerm) {
+    const filteredResult = dataArray.filter(function(element) {
+        return element.name.includes(searchTerm);
+    });
+    return filteredResult;
+}
